caa9dcc12a89e61e3fb9b5eba93bb2553a4e7f99
leesungkyu1 <lccab2002@gmail.com>
2022-11-02 09:56:36 +0900

    war build

--- src/main/java/com/reactpractice/lee/LeeApplication.java
+++ src/main/java/com/reactpractice/lee/LeeApplication.java
@@ -2,10 +2,17 @@ package com.reactpractice.lee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LeeApplication {
+public class LeeApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(LeeApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+		return builder.sources(LeeApplication.class);
 	}
 }
